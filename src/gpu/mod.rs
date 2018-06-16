6aacff4524399d7c9f26f4c7497cd429b798c69d
Hilmar Wiegand <hilmar.wiegand@flameshome.net>
2018-06-17 01:31:43 +0200

    :construction: :hankey: Render background map badly

--- src/gpu/mod.rs
+++ src/gpu/mod.rs
@@ -1,3 +1,6 @@
+use std::io;
+use std::io::prelude::*;
+
 #[derive(Clone, Copy, Debug)]
 pub enum GpuMode {
   // blanks
@@ -12,7 +15,6 @@ pub enum GpuMode {
 pub struct Gpu {
   pub mode: GpuMode,
   pub vram: Vec<u8>,
-  pub tiles: Box<[Tile; 384]>,
 
   pub vblank_interrupt: bool,
 
@@ -20,17 +22,8 @@ pub struct Gpu {
   pub line: u8,
 
   pub enabled: bool,
-}
 
-#[derive(Copy, Clone, Debug)]
-pub struct Tile {
-  pub pixels: [u8; 16],
-}
-
-impl Tile {
-  pub fn new() -> Tile {
-    Tile { pixels: [0u8; 16] }
-  }
+  pub back_buffer: Box<[u8; 32 * 32]>,
 }
 
 impl Gpu {
@@ -38,12 +31,11 @@ impl Gpu {
     let mode = GpuMode::HBlank;
 
     // TODO: How big is this supposed to be?
-    let vram = (0..=0x2000).map(|_| 0x99).collect::<Vec<u8>>();
+    let vram = (0..=0x3000).map(|_| 0x99).collect::<Vec<u8>>();
 
     Gpu {
       mode,
       vram,
-      tiles: Box::new([Tile::new(); 384]),
 
       vblank_interrupt: false,
 
@@ -51,29 +43,79 @@ impl Gpu {
       line: 0,
 
       enabled: false,
+      back_buffer: Box::new([0; 32 * 32]),
+    }
+  }
+
+  pub fn render_screen_to_texture(&self, texture: &mut Vec<u8>) {
+    for (row_index, row) in self.back_buffer.chunks(32).enumerate() {
+      let y_component = row_index * 8 * 256 * 3;
+
+      for (tile_index, tile) in row.iter().enumerate() {
+        let x_component = tile_index * 8 * 3;
+
+        let vram_offset = (*tile as usize) * 16;
+        let tile_row_indices = (vram_offset..(vram_offset + 16)).collect::<Vec<usize>>();
+        for (tile_row_index, tile_row) in tile_row_indices.chunks(2).enumerate() {
+          for i in 0..8 {
+            let low = (self.vram[tile_row[0]] >> (7 - i)) & 1;
+            let high = (self.vram[tile_row[1]] >> (7 - i)) & 1;
+            let color = low | (high << 1);
+
+            let tile_x = i * 3;
+            let tile_y = tile_row_index * 256 * 3;
+
+            let texture_index = (
+              y_component +
+              x_component +
+              tile_x +
+              tile_y
+            );
+
+            match color {
+              0 => {
+                texture[texture_index] = 230;
+                texture[texture_index + 1] = 230;
+                texture[texture_index + 2] = 230;
+              },
+              1 => {
+                texture[texture_index] = 160;
+                texture[texture_index + 1] = 160;
+                texture[texture_index + 2] = 160;
+              },
+              2 => {
+                texture[texture_index] = 80;
+                texture[texture_index + 1] = 80;
+                texture[texture_index + 2] = 80;
+              },
+              3 => {
+                texture[texture_index] = 0;
+                texture[texture_index + 1] = 0;
+                texture[texture_index + 2] = 0;
+              },
+              it => println!("invalid color: {}", it),
+            };
+          }
+        }
+      }
+    }
+  }
+
+  fn render_line(&mut self) {
+    if self.line >= 32 { return; }
+
+    let offset = (self.line as u16 * 32) as usize;
+    let vram_offset = 0x1800 + offset;
+    for (index_offset, i) in (vram_offset..(vram_offset + 32)).enumerate() {
+      self.back_buffer[offset + index_offset] = self.vram[i];
     }
   }
 
   pub fn render_vram_to_texture(&self, texture: &mut Vec<u8>) {
-    // let mut tile = (0..(8 * 2)).map(|_| 0u8).collect::<Vec<u8>>();
 
-    // tile[1] |= 0x7E;
-    // tile[2] |= 0x7E;
-    // tile[4] |= 0x7;
-    // tile[5] |= 0x7;
-    // tile[6] |= 0xE0;
-    // tile[7] |= 0xE0;
-    // tile[8] |= 0x6;
-    // tile[9] |= 0x60;
-    // tile[12] |= 0x3F;
-    // tile[13] |= 0x3F;
-    // tile[14] |= 0xF;
-    // tile[15] |= 0xF;
-
-    let mut acc = 0;
     for (tile_index, tile) in self.vram.chunks(16).enumerate() {
-      let texture_row = acc / 16;
-      let texture_row_offset = texture_row * 128 * 3 * 8;
+      let texture_row = tile_index / 16;
+      let texture_row_offset = texture_row * 128 * 3 * 7;
 
       for (row_index, row) in tile.chunks(2).enumerate() {
         for i in 0..=7 {
@@ -115,56 +157,8 @@ impl Gpu {
         }
       }
 
-      acc += 1;
-      if acc > 384 { break; }
+      if tile_index >= 384 { break; }
     }
-
-    // for (tile_index, tile) in self.tiles.iter().enumerate() {
-    // for (tile_index, tile) in self.vram.chunks(16).enumerate() {
-    //   if tile_index * 16 > 0x17FF { break; }
-
-    //   // two bytes per row
-    //   // for (row, pixel) in tile.pixels.chunks(2).enumerate() {
-    //   for (row, row_pixels) in tile.chunks(2).enumerate() {
-
-    //     for i in 0..=7 {
-    //       let low = (row_pixels[0] >> i) & 1;
-    //       let high = (row_pixels[1] >> i) & 1;
-
-    //       let color = low | (high << 1);
-
-    //       let tile_row = tile_index % 4;
-    //       let y_offset = (tile_index / 4) * 256 * 3;
-
-    //       let position = (row * 256 * 3) + (tile_row * 8 * 3) + (i * 3) + y_offset;
-    //       if position >= texture.len() { break; }
-
-    //       match color {
-    //         0 => {
-    //           texture[position] = 230;
-    //           texture[position + 1] = 230;
-    //           texture[position + 2] = 230;
-    //         },
-    //         1 => {
-    //           texture[position] = 160;
-    //           texture[position + 1] = 160;
-    //           texture[position + 2] = 160;
-    //         },
-    //         2 => {
-    //           texture[position] = 80;
-    //           texture[position + 1] = 80;
-    //           texture[position + 2] = 80;
-    //         },
-    //         3 => {
-    //           texture[position] = 0;
-    //           texture[position + 1] = 0;
-    //           texture[position + 2] = 0;
-    //         },
-    //         it => println!("invalid color: {}", it),
-    //       };
-    //     }
-    //   }
-    // }
   }
 
   pub fn step(&mut self, cycles: u32) {
@@ -222,7 +216,7 @@ impl Gpu {
         self.clock = 0;
         self.mode = GpuMode::HBlank;
 
-        // write to buffer here
+        self.render_line();
       },
     };
   }
